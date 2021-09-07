2a9237b4d4a758f84e4f5dd583bef60a4b359ea9
MasterfulPleb <justinmunger027@yahoo.com>
2021-09-07 11:26:15 -0400

    1.1.32

--- scraper.js
+++ scraper.js
@@ -3,26 +3,27 @@ const mariadb = require('mariadb');
 const snoowrap = require('snoowrap');
 const login = require('./login.json');
 const r = new snoowrap(login);
-const start = 'hbxr2az';//ID of starting comment
+const start = 'hbxr2az'; //ID of starting comment
 
-main()
+main();
 
 //it doesnt need to be a function i guess but it looks more professional
-function main() {
+async function main() {
     var nextComment = start;
+    var commentCount
     var conn;
     try {
+        //connects to MariaDB
+        conn = await mariadb.createConnection({
+            socketPath: '/var/run/mysqld/mysqld.sock',
+            user: 'root',
+            database: 'bee_movie'
+        });
         //the meat of the script
         //goes up the chain adding every comment in the main thread to the DB
-        var pushComment = setInterval(async function() {//stop at top level
+        var pushComment = setInterval(async function() {
             //get next comment
             var c = await r.getComment(nextComment).fetch();
-            //connects to MariaDB
-            conn = await mariadb.createConnection({
-                socketPath: '/var/run/mysqld/mysqld.sock',
-                user: 'root',
-                database: 'bee_movie'
-            });
             //push comment to DB
             conn.query('INSERT INTO comments ' +
                 '(ID,body,author,timestamp,parentID,permalink,edited,OP,awards)' +
@@ -31,15 +32,15 @@ function main() {
                 (c.edited > 0 ? 1: 0) + ',' + c.is_submitter + ',' + c.total_awards_received + ');'
             );
             nextComment = c.parent_id.slice(3);
+            console.log('comments recorded:' + commentCount);
             console.log('ratelimit remaining: ' + r.ratelimitRemaining);
-            console.log('ratelimit expiration: ' + r.ratelimitExpiration);
-            if (nextComment == 'ofiegh') clearInterval(pushComment)
+            if (nextComment == 'ofiegh') {
+                conn.close(); //close the MariaDB connection
+                clearInterval(pushComment); //stop at top comment
+            }
         }, 1020);
     } catch (err) {
         //error handling
         console.log(err);
-    } finally {
-        //close the MariaDB connection
-        if (conn) conn.close();
     }
 }
