38c47c54fb9be872ef96dfd04c3ddf9e06465cb2
MasterfulPleb <justinmunger027@yahoo.com>
2021-09-07 15:43:31 -0400

    1.2.8

--- scraper.js
+++ scraper.js
@@ -3,7 +3,7 @@ const mariadb = require('mariadb');
 const snoowrap = require('snoowrap');
 const login = require('./login.json');
 const r = new snoowrap(login);
-const start = 'h9w33ln'; //ID of starting comment
+const start = 'h99s4fh'; //ID of starting comment
 
 main();
 
